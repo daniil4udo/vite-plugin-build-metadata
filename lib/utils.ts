6eb8dd513e125554d261503fcc08c8b0623e078a
daniil4udo <daniil4udo@gmail.com>
2025-02-02 10:21:21 +0400

    refactor: use latest Node 22 hash()

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,25 @@
 import childProcess from 'node:child_process';
-import { createHash } from 'node:crypto';
+import crypto from 'node:crypto';
 
 import { createLogger } from 'vite';
 
 const logger = createLogger();
 
-// https://github.com/vitejs/vite/blob/d6bde8b03d433778aaed62afc2be0630c8131908/packages/vite/src/node/utils.ts#L990C17-L990C24
-export function getHash(text: globalThis.Buffer | string) {
+const hash
+  // eslint-disable-next-line n/no-unsupported-features/node-builtins -- crypto.hash is supported in Node 21.7.0+, 20.12.0+
+  = crypto.hash
+      ?? ((
+          algorithm: string,
+          data: crypto.BinaryLike,
+          outputEncoding: crypto.BinaryToTextEncoding,
+      ) => crypto.createHash(algorithm).update(data).digest(outputEncoding));
+
+export function getHash(text: Buffer | string) {
     try {
-        return createHash('sha256')
-            .update(text)
-            .digest('hex')
-            .substring(0, 8);
+        const h = hash('sha256', text, 'hex').substring(0, length);
+        if (length <= 64)
+            return h;
+        return h.padEnd(length, '_');
     }
     catch (e) {
         logger.error(`[getHash] - Error while creating hash: ${e}`);
